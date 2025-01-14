bc14be1f3d9eb20e815c709f0244351acf5eb2eb
Árgar Crespo <argar.crespo.7e8@itb.cat>
2025-01-14 11:39:28 +0100

    Update Utilities.kt

--- Utilidades_LOK/Utilities.kt
+++ Utilidades_LOK/Utilities.kt
@@ -491,3 +491,205 @@ fun aplanarArbre(node: Node): List<Int> {
 
     return llistaArbre
 }
+
+/**
+ * Document kt dedicat al emmagatzematge de funcions de lectura de Int, Double, Float, Long, Word, Char per poder usar
+ * en les funcions dels projectes.
+ * @author Raimon Izard, David Marin i Martí Vilàs.
+ */
+fun llegirInt(pMessageIn: String
+              , pMessageErrorDT: String
+): Int{
+    val scanner=Scanner(System.`in`)
+    var num:Int=0
+    var error = true
+    do {if (scanner.hasNextInt()){
+        num=scanner.nextInt()
+        error=false
+    }else {
+        scanner.nextLine()
+        println("${RED}ERROR HA DE SER UN NÚMERO ENTER${RESET}")
+    }
+    }while (error)
+    return num
+}
+
+fun llegirDouble(pMessageIn: String
+                 , pMessageErrorDT: String
+):Double {
+    val scanner=Scanner(System.`in`)
+    var num:Double=0.0
+    var error = true
+    do {if (scanner.hasNextDouble()){
+        num=scanner.nextDouble()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+    return num
+}
+
+/**
+ * This method can be used to read an Int value from the user through keyboard using java.util.Scanner
+ * @author argar.crespo
+ * @since 11/12/2024
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+fun llegirFloat( pMessageIn: String, pMessageErrorDT: String):Float {
+    var outputValue: Float = 0.0f
+    var correctDataType: Boolean = false
+
+    do{
+        println(PURPLE_BOLD + pMessageIn + RESET)
+        correctDataType = scan.hasNextFloat()
+
+        if (!correctDataType){
+            println(RED_BACKGROUND_BRIGHT + "ERROR: " + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.nextFloat()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+fun llegirLong():Long {
+    val scanner=Scanner(System.`in`)
+    var num:Long=0
+    var error = true
+    do {if (scanner.hasNextLong()){
+        num=scanner.nextLong()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+    return num
+}
+
+fun llegirWord(pMessageIn: String, pMessageErrorDT: String): String {
+    val scanner=Scanner(System.`in`)
+    val outputValue: String
+    do {
+        println(pMessageIn)
+        if (!scanner.hasNext()) {
+            println("ERROR: $pMessageErrorDT")
+            scanner.next()
+        } else {
+            outputValue = scanner.next()
+            scanner.nextLine()
+            return outputValue
+        }
+    } while (true)
+}
+
+fun llegirChar( pCase: Int = -1): Char {
+    val scanner=Scanner(System.`in`)
+    val outputValue: Char
+    do {
+        if (!scanner.hasNext()) {
+            scanner.next()
+        } else {
+            outputValue = scanner.next()[0]
+            scanner.nextLine() // clear buffer
+            when (pCase) {
+                0 -> return outputValue.lowercaseChar()
+                1 -> return outputValue.uppercaseChar()
+                else -> return outputValue
+            }
+        }
+    } while (true)
+}
+
+fun llegirStrings(pMessageIn: String, pMessageErrorDT: String): String{
+    val scanner=Scanner(System.`in`)
+
+    var outputValue: String = ""
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scanner.hasNext()
+
+        if (!correctDataType){
+            println(RED_BACKGROUND_BRIGHT + "ERROR: " + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scanner.nextLine()
+        }
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+
+fun llegirSioNo(pMessageIn: String, pMessageErrorDT: String) : Boolean {
+    var scanner=Scanner(System.`in`)
+    val resposta=scanner.nextLine().lowercase()
+
+    return when (resposta) {
+        "si" -> true
+        "s" -> true
+        "no" -> false
+        "n" -> false
+        else -> {
+            println("Resposta no vàlida la resposta ha de ser Si o No")
+            llegirSioNo(pMessageIn, pMessageErrorDT)
+        }
+    }
+}
+
+fun llegirEntre1o2(): Int {
+    val scanner = Scanner(System.`in`)
+    var numero: Int = 0
+    var error = true
+
+    do {
+        if (scanner.hasNextInt()) {
+            numero = scanner.nextInt()
+            if (numero in 1..2) {
+                error = false
+            } else {
+                println("Per favor, ingressa un valor vàlid (1 o 2).")
+            }
+        } else {
+            println("Per favor, ingressa un número enter.")
+            scanner.nextLine()
+        }
+    } while (error)
+    return numero
+}
+
+/**
+ * This method can be used to read an Int value from the user through keyboard using java.util.Scanner
+ * @author argar.crespo
+ * @since 11/12/2024
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+
+fun llegirTotsInt(pMessageIn: String
+            , pMessageErrorDT: String
+): Int{
+
+    var valorDeSalida: Int = 0
+    var correcto: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correcto = scan.hasNextInt()
+
+        if (!correcto){
+            println(RED_BACKGROUND_BRIGHT + "ERROR: " + pMessageErrorDT + RESET)
+        }else{
+            valorDeSalida = scan.nextInt()
+        }
+        scan.nextLine()
+    }while(!correcto)
+
+    return valorDeSalida
+}
